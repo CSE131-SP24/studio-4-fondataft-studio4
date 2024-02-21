7b0ee23bb0a5a1a2eda529d7c715d4d51604edfc
annafonda <annafonda@localhost>
2024-02-21 14:17:21 -0600

    studio done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,24 @@
 package studio4;
 
-import java.awt.Color;
-
 import edu.princeton.cs.introcs.StdDraw;
 
-public class Flag {
+public class Flag {		
 	public static void main(String[] args) {
-		
+			
+			StdDraw.setXscale(0, 24);
+			StdDraw.setYscale(0, 16);
+			
+			StdDraw.clear();
+			StdDraw.setPenColor(245, 128, 37);
+			StdDraw.filledRectangle(5, 3, 5, 3);
+			StdDraw.filledRectangle(5, 13, 5, 3);
+			StdDraw.filledRectangle(19, 3, 5, 3);
+			StdDraw.filledRectangle(19, 13, 5, 3);
+			StdDraw.setPenColor(88, 0, 156);
+			StdDraw.filledRectangle(12, 8, 12, 2);
+			StdDraw.filledRectangle(12, 8, 2, 8);
+			
 	}
-}
+
+			
+	}
--- src/studio4/NorwayMotherOfAllFlags.java
+++ src/studio4/NorwayMotherOfAllFlags.java
@@ -30,7 +30,19 @@ public class NorwayMotherOfAllFlags {
 
 	public static void france() {
 		
-		// TODO
+		StdDraw.clear();
+		StdDraw.setPenColor(255, 0, 0);
+		StdDraw.filledRectangle(3, 3, 3, 3);
+		StdDraw.filledRectangle(3, 13, 3, 3);
+		StdDraw.filledRectangle(16, 3, 6, 3);
+		StdDraw.filledRectangle(16, 13, 6, 3);
+		StdDraw.setPenColor(0, 0, 127);
+		StdDraw.filledRectangle(11, 8, 11, 1);
+		StdDraw.filledRectangle(8, 8, 1, 8);
+		
+		StdDraw.setXscale(0, 22);
+		StdDraw.setYscale(0, 16);
+		mother();
 	}
 
 	public static void indonesia() {
